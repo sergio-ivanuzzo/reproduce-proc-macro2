0002ff60742e02e79e701e8882e7b0b8cc7999ac
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2024-04-21 19:10:56 +0200

    updates

--- src/macro/src/lib.rs
+++ src/macro/src/lib.rs
@@ -1,17 +1,36 @@
 use proc_macro::{TokenStream};
+use std::collections::BTreeMap;
 use proc_macro2::{Ident};
 use syn::{Attribute, ItemStruct};
 use syn::{parse_macro_input, ExprArray};
 use quote::{quote, format_ident};
+use quote::__private::ext::RepToTokensExt;
 use syn::Expr;
 use syn::Token;
 use syn::bracketed;
 use syn::punctuated::Punctuated;
+use syn::Meta;
+use syn::Lit;
+use syn::parse::Parse;
+use syn::parse::ParseStream;
+
+struct MyAttr {
+    pub name: Ident,
+}
+
+impl Parse for MyAttr {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let name: Ident = input.parse()?;
+
+        Ok(Self { name })
+    }
+}
 
 #[proc_macro_derive(Segment, attributes(depends_on))]
 pub fn derive_segment(input: TokenStream) -> TokenStream {
-    let ItemStruct { ident, fields, .. } = parse_macro_input!(input);
+    let ItemStruct { ident, fields, attrs, .. } = parse_macro_input!(input);
     let binary_converter = quote! { BinaryConverter };
+    let to_bytes = quote! { ToBytes };
     let cursor = quote! { std::io::Cursor };
 
     let field_names = fields.iter().map(|f| {
@@ -23,23 +42,59 @@ pub fn derive_segment(input: TokenStream) -> TokenStream {
         quote! { self.#field_name }
     }).collect::<Vec<_>>();
 
-    let mut depends_on = vec![];
+    let mut depends_on: BTreeMap<Option<Ident>, Vec<Ident>> = BTreeMap::new();
 
     for field in fields.iter() {
-        let ident = field.ident.as_ref().unwrap().to_string();
+        let ident = field.ident.clone();
 
         if field.attrs.iter().any(|attr| attr.path().is_ident("depends_on")) {
-            depends_on.push(Some(ident));
+
+            let mut dependencies: Vec<Ident> = vec![];
+            field.attrs.iter().for_each(|attr| {
+                for a in attr.parse_args_with(Punctuated::<MyAttr, Token![,]>::parse_terminated).unwrap() {
+                    dependencies.push(a.name);
+                }
+            });
+
+            depends_on.insert(ident, dependencies);
         }
     }
 
     let initializers = fields.iter().map(|f| {
-        quote! { #binary_converter::read_from(&mut reader, vec![]).unwrap() }
+        let field_name = f.ident.clone();
+        let field_type = f.ty.clone();
+
+        if let Some(dependencies) = depends_on.get(&field_name) {
+            quote! {
+                {
+                    let mut buffer: Vec<u8> = vec![];
+                    #(
+                        let bytes = #to_bytes::to_bytes(
+                            &mut cache.#dependencies,
+                        );
+                        buffer.extend(bytes);
+                    )*
+                    #binary_converter::read_from(&mut reader, buffer).unwrap()
+                }
+            }
+        } else {
+            quote! {
+                {
+                    let value: #field_type = #binary_converter::read_from(&mut reader, vec![]).unwrap();
+                    cache.#field_name = value.clone();
+                    value
+                }
+            }
+        }
     });
 
     let output = quote! {
         impl #ident {
             pub fn from_binary(data: Vec<u8>) -> Self {
+                let mut cache = Self {
+                    #(#field_names: Default::default()),*
+                };
+
                 let mut reader = #cursor::new(data);
 
                 let mut instance = Self {
@@ -50,7 +105,8 @@ pub fn derive_segment(input: TokenStream) -> TokenStream {
             }
 
             pub fn test(&mut self) {
-                println!("{:?}", vec![#(&#depends_on),*]);
+                // println!("{:?}", vec![#(&#depends_on),*]);
+                // println!("{:?}", #valyes);
             }
         }
     };
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+use std::collections::BTreeMap;
 use tentacli_packet::Segment;
 use std::io::{BufRead, Error, ErrorKind, Read, Write};
-use byteorder::{ReadBytesExt, WriteBytesExt};
+use std::mem;
+use bitflags::bitflags;
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 
 #[derive(Segment)]
 struct Test {
-    a: u8,
-    b: u8,
+    a: u32,
+    b: u32,
     #[depends_on(a, b)]
-    c: u8,
+    c: u32,
     #[depends_on(a, b, c)]
-    d: u8,
-    e: u8,
-    f: u8,
+    d: u32,
+    e: u32,
+    f: u32,
 }
 
 pub trait BinaryConverter {
@@ -32,7 +35,111 @@ impl BinaryConverter for u8 {
     }
 }
 
+impl BinaryConverter for u32 {
+    fn write_into(&self, buffer: &mut Vec<u8>) -> Result<(), Error> {
+        buffer.write_u32::<LittleEndian>(*self)
+    }
+
+    fn read_from<R: BufRead>(mut reader: R, dependencies: Vec<u8>) -> Result<Self, Error> {
+        println!("u32 DEPS: {:?}", dependencies);
+        reader.read_u32::<LittleEndian>()
+    }
+}
+
+trait ToBytes {
+    fn to_bytes(&self) -> Vec<u8>;
+}
+
+impl ToBytes for i32 {
+    fn to_bytes(&self) -> Vec<u8> {
+        unsafe {
+            let size = mem::size_of::<i32>();
+            let ptr = self as *const i32 as *const u8;
+            let mut bytes = Vec::with_capacity(size);
+            for i in 0..size {
+                bytes.push(*ptr.add(i));
+            }
+            bytes
+        }
+    }
+}
+
+impl ToBytes for u32 {
+    fn to_bytes(&self) -> Vec<u8> {
+        unsafe {
+            let size = mem::size_of::<u32>();
+            let ptr = self as *const u32 as *const u8;
+            let mut bytes = Vec::with_capacity(size);
+            for i in 0..size {
+                bytes.push(*ptr.add(i));
+            }
+            bytes
+        }
+    }
+}
+
+impl ToBytes for f64 {
+    fn to_bytes(&self) -> Vec<u8> {
+        unsafe {
+            let size = mem::size_of::<f64>();
+            let ptr = self as *const f64 as *const u8;
+            let mut bytes = Vec::with_capacity(size);
+            for i in 0..size {
+                bytes.push(*ptr.add(i));
+            }
+            bytes
+        }
+    }
+}
+
+impl ToBytes for bool {
+    fn to_bytes(&self) -> Vec<u8> {
+        if *self {
+            vec![1u8]
+        } else {
+            vec![0u8]
+        }
+    }
+}
+
+impl<T: ToBytes> ToBytes for Vec<T> {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+        for item in self {
+            bytes.extend_from_slice(&item.to_bytes());
+        }
+        bytes
+    }
+}
+
+impl<K: ToBytes + Ord, V: ToBytes> ToBytes for BTreeMap<K, V> {
+    fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+        for (key, value) in self {
+            bytes.extend_from_slice(&key.to_bytes());
+            bytes.extend_from_slice(&value.to_bytes());
+        }
+        bytes
+    }
+}
+
+bitflags! {
+    struct Flags: u32 {
+        const FLAG_A = 0b00000001;
+        const FLAG_B = 0b00000010;
+        const FLAG_C = 0b00000100;
+    }
+}
+
+impl ToBytes for Flags {
+    fn to_bytes(&self) -> Vec<u8> {
+        self.bits().to_bytes()
+    }
+}
+
 fn main() {
-    let data = vec![1, 2, 3, 4, 5, 6];
-    Test::from_binary(data).test();
+    let data: Vec<u32> = vec![1, 2, 3, 4, 5, 6];
+    let data_u8: Vec<u8> = data.iter().flat_map(|&num| num.to_le_bytes().to_vec()).collect();
+
+    Test::from_binary(data_u8).test();
 }
